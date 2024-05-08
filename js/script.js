6b51a066f2f5b520f99560114521ae369bcbfa63
Johan Frederic Aas-Haug <91760515+Zarden92@users.noreply.github.com>
2024-05-08 21:34:29 +0200

    fixed error with check task not working properly in localStorage

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ listContainer.addEventListener(
   function (e) {
     if (e.target.tagName === "LI") {
       e.target.classList.toggle("checked");
+      saveData();
     } else if (e.target.tagName === "SPAN") {
       e.target.parentElement.remove();
     }
